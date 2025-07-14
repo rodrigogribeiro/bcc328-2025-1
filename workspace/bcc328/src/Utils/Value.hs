e2b9d2bfb3da94d295fb2c640aa82ee22160010b
Rodrigo Ribeiro <rodrigogribeiro@gmail.com>
2025-07-14 19:32:37 -0300

    adding more operations on values
    Signed-off-by: Rodrigo Ribeiro <rodrigogribeiro@gmail.com>

--- workspace/bcc328/src/Utils/Value.hs
+++ workspace/bcc328/src/Utils/Value.hs
@@ -1,5 +1,6 @@
 module Utils.Value where
 
+import Data.Char
 import Utils.Pretty
 
 data Value
@@ -28,3 +29,51 @@ e1 .-. e2 = Left $ unwords ["Type error on:", pretty e1, "-", pretty e2]
 (./.) :: Value -> Value -> Either String Value
 (VInt n1) ./. (VInt n2) = Right (VInt (n1 `div` n2))
 e1 ./. e2 = Left $ unwords ["Type error on:", pretty e1, "/", pretty e2]
+
+(.<.) :: Value -> Value -> Either String Value
+(VInt n1) .<. (VInt n2) = Right (VBool (n1 < n2))
+e1 .<. e2 = Left $ unwords ["Type error on:", pretty e1, "<", pretty e2]
+
+(.=.) :: Value -> Value -> Either String Value
+(VInt n1) .=. (VInt n2) = Right (VBool (n1 == n2))
+(VBool b1) .=. (VBool b2) = Right (VBool (b1 == b2))
+(VStr s1) .=. (VStr s2) = Right (VBool (s1 == s2))
+e1 .=. e2 = Left $ unwords ["Type error on:", pretty e1, "=", pretty e2]
+
+catValue :: Value -> Value -> Either String Value
+catValue (VStr s1) (VStr s2) = Right (VStr (s1 ++ s2))
+catValue e1 e2 = Left $ unwords ["Type error on: strcat(", pretty e1, ",", pretty e2, ")"]
+
+catSize :: Value -> Either String Value
+catSize (VStr s1) = Right (VInt (length s1))
+catSize e1 = Left $ unwords ["Type error on: strsize(", pretty e1, ")"]
+
+i2s :: Value -> Either String Value
+i2s (VInt n1) = Right (VStr (show n1))
+i2s e1 = Left $ unwords ["Type error on: i2s(", pretty e1, ")"]
+
+i2b :: Value -> Either String Value
+i2b (VInt n1) = Right (VBool (n1 /= 0))
+i2b e1 = Left $ unwords ["Type error on: i2b(", pretty e1, ")"]
+
+b2i :: Value -> Either String Value
+b2i (VBool b) = Right $ if b then VInt 1 else VInt 0
+b2i e1 = Left $ unwords ["Type error on: b2i(", pretty e1, ")"]
+
+b2s :: Value -> Either String Value
+b2s (VBool b) = Right $ if b then VStr "true" else VStr "false"
+b2s e1 = Left $ unwords ["Type error on: b2s(", pretty e1, ")"]
+
+s2i :: Value -> Either String Value
+s2i e1@(VStr s)
+  | all isDigit s && (not $ null s) = Right $ VInt (read s)
+  | otherwise = Left $ unwords ["Type error on : s2i(", pretty e1, ")"]
+s2i e1 = Left $ unwords ["Type error on : s2i(", pretty e1, ")"]
+
+s2b :: Value -> Either String Value
+s2b e1@(VStr s)
+  | s `elem` ["true", "false"] = Right $ VBool (read s)
+  | otherwise = Left $ unwords ["Type error on : s2b(", pretty e1, ")"]
+s2b e1 = Left $ unwords ["Type error on : s2b(", pretty e1, ")"]
+
+
